2a4f6a24ba82d1717f79a7cbdb4846a51cc694a4
CharleneM <charlenemarceau@hotmail.fr>
2021-07-24 14:34:37 +0200

    migration

--- migrations/Version20210724123135.php
+++ migrations/Version20210724123135.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210723135831 extends AbstractMigration
+final class Version20210724123135 extends AbstractMigration
 {
     public function getDescription(): string
     {
